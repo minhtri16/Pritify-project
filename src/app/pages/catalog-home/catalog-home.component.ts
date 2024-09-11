682ce36e7ab871413373f39fd6ccc62c007d7d26
minhtri16 <triduc201@gmail.com>
2024-09-11 15:56:32 +0700

    update github

---
+++ src/app/pages/catalog-home/catalog-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './catalog-home.component.html',
+  styleUrl: './catalog-home.component.css'
+})
+export class CatalogHomeComponent {
+
+}
